b2b9cf494fb7065624185252c561078dbf936dac
Pengfei Zhang <pengfei.zhang@exchange.co.jp>
2015-12-21 23:19:35 +0900

    change ○ to $

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -5,7 +5,7 @@ function virtualenv_info {
 function prompt_char {
     git branch >/dev/null 2>/dev/null && echo '±' && return
     hg root >/dev/null 2>/dev/null && echo '☿' && return
-    echo '○'
+    echo '$'
 }
 
 function box_name {
