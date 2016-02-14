c447838372971547af32fc3be20db75d63c19683
Pengfei Zhang <pfzhang1987@gmail.com>
2016-02-14 21:39:31 +0900

    Remove rsa key and modify aliase

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -26,7 +26,7 @@ alias 'bk=cd $OLDPWD'
 # -------------------------------------------------------------------
 alias lh='ls -d .*' # show hidden files/directories only
 alias lsd='ls -aFhlG'
-alias l='ls -al'
+alias l='ls -altr'
 alias ls='ls -GFh' # Colorize output, add file type indicator, and put sizes in human readable format
 alias ll='ls -GFhl' # Same as above, but in long listing format
 alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'"
