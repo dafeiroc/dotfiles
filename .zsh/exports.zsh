aa37419728e70a0e9b819a61665a097a0b6b9fa0
Pengfei Zhang <pfzhang1987@gmail.com>
2016-08-30 12:10:52 +0900

    update

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -24,7 +24,9 @@ export EDITOR='subl -w'
 #export NODE_PATH=/opt/github/homebrew/lib/node_modules
 #export PYTHONPATH=/usr/local/lib/python2.6/site-packages
 # CTAGS Sorting in VIM/Emacs is better behaved with this in place
-export LC_COLLATE=C 
+export LC_ALL=en_US.UTF-8  
+export LANG=en_US.UTF-8
+#export LC_COLLATE=C 
 
 #export GH_ISSUE_CREATE_TOKEN=083f60c674d8eb41f98258df9fc8d94cb733218a
 
